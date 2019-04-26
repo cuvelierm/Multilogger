a253f0bd1d8ed76fd312986cc5f63cb363aeef30
Michiel Leunens <michiel.leunens@gmail.com>
2019-04-26 22:02:27 +0200

    somethin somethin

--- logger/MultiloggerWare.js
+++ logger/MultiloggerWare.js
@@ -1,8 +1,18 @@
 const multilogger = {
-  multilog: async (req, res, next) => {
-      console.log(`Logged  ${req.url}  ${req.method} -- ${new Date()}`);
+  multilog: async (req, res, next, err) => {
+
+
+      if (err) {
+          console.log(err.stack);
+      }
+
+      console.log(`Logged ${req.url}  ${req.method} -- ${new Date()}`);
       next();
-  }
+  },
+
+    // basic: async (res, req, next) => {
+    //
+    // },
 };
 
 module.exports = multilogger;
